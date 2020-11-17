f454ed9a222f8acc37d5f454b16190d9c1ba26d1
ise <ise@ubuntu>
2020-11-17 10:51:04 -0800

    first

---
+++ Rick&Morty/src/Planet.c
@@ -0,0 +1,8 @@
+/*
+ * Planet.c
+ *
+ *  Created on: Nov 17, 2020
+ *      Author: ise
+ */
+
+
---
+++ Rick&Morty/src/Planet.h
@@ -0,0 +1,13 @@
+/*
+ * Planet.h
+ *
+ *  Created on: Nov 17, 2020
+ *      Author: ise
+ */
+
+#ifndef PLANET_H_
+#define PLANET_H_
+
+
+
+#endif /* PLANET_H_ */
